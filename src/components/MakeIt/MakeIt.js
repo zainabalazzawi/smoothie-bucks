6b4943bb3eccfbe2d595979012d744df7ccfb789
zainabalazzawi <zainabalazzawi91@gmail.com>
2021-10-07 16:02:49 +0300

    Add px value to logo

--- src/components/MakeIt/MakeIt.js
+++ src/components/MakeIt/MakeIt.js
@@ -7,9 +7,12 @@ const MakeIt = ({ selectedList, yourName, setGetIt }) => {
     <div className="make-it-container">
       <div className="make-it-logo"> </div>
       <div className="text-3">Enjoy your Smoothie!</div>
-      <div className="make-it-cup">
-        <SmoothieCup selectedList={selectedList} yourName={yourName} />
-      </div>
+      <SmoothieCup
+        className="make-it-cup"
+        selectedList={selectedList}
+        yourName={yourName}
+      />
+
       <div className="make-new-one" onClick={() => setGetIt(false)}>
         Make a new one
       </div>
